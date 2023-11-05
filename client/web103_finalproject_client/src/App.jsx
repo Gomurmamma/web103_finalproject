114a2511233015d9037125fa7909184d691daec7
Donald Antonio Young <youngdo@oregonstate.edu>
2023-11-05 16:23:56 -0500

    Defined, imported PageNotFound page to app routing.

--- client/web103_finalproject_client/src/App.jsx
+++ client/web103_finalproject_client/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+import {useRoutes} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path:"/*",
+      element: <PageNotFound />
+    }
+  ]);
 
   return (
     <>
