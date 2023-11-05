479ce08ba283ee7524abc1c1426f6a2f0eca0405
Donald Antonio Young <youngdo@oregonstate.edu>
2023-11-05 16:30:52 -0500

    defined App header layout and nav along with page element.

--- client/web103_finalproject_client/src/App.jsx
+++ client/web103_finalproject_client/src/App.jsx
@@ -7,7 +7,6 @@ import './App.css'
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   // Sets up routes
   let element = useRoutes([
@@ -19,26 +18,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header>
+      <nav>
+        <Link to="/"><h1>SoundSphere</h1></Link>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      {element}
+    </main>
     </>
   )
 }
