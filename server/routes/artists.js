500bcb6da6b7cc32f0bcfe0ed4b783bbf8261522
Jerry Shi <jerryshi25@gmail.com>
2023-11-06 21:32:36 -0500

    added artists routes

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -3,6 +3,14 @@ import ArtistsController from '../controllers/artists.js'
 
 const router = express.Router()
 
+router.get('/', ArtistsController.getArtists)
 
+router.get('/:id', ArtistsController.getArtist)
+
+router.post('/', ArtistsController.createArtist)
+
+router.delete('/:id', ArtistsController.deleteArtist)
+
+router.patch('/:id', ArtistsController.updateArtist)
 
 export default router
