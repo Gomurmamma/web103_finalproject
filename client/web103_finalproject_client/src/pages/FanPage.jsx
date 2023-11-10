07dbe3094f7b957ec90dc830e14ea84f96d03f5f
Donald Antonio Young <youngdo@oregonstate.edu>
2023-11-09 20:48:42 -0500

    defined fan and release state with services

--- client/web103_finalproject_client/src/pages/FanPage.jsx
+++ client/web103_finalproject_client/src/pages/FanPage.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import AlbumOverview from "../components/AlbumOverview";
+import { useParams } from "react-router-dom";
 
-const FanPage = ({fanData, customerReleases}) => {
+const FanPage = ({}) => {
+  const { id } = useParams();
+
+  const [fan, setFan] = useState({
+    name: "",
+    imageURL: "",
+    location: "",
+    description: "",
+  });
+
+  
+
+  const [customerReleases, setCustomerReleases] = useState([]);
+
+
+  useEffect(() => {
+    async function startFetching(){
+        const customerReleaseData = await releasesServices.fetchReleasesByCustomerID(id.toString());
+        const fanData = await fanServices.fetchFanById(id);
+  
+        if (!stopFetch){
+          setCustomerReleases(customerReleaseData);
+          setFan(fanData);
+        }
+      }
+  
+      let stopFetch = false;
+      startFetching();
+      return () => {
+        stopFetch = true;
+      }
+
+}, [id]);
+
   return (
     <section>
       <figure>
-        <img src={fanData.imageURL} alt={fanData.name + `'s profile image`}></img>
+        <img src={fan.imageURL} alt={fan.name + `'s profile image`}></img>
         <figcaption>
-            <h2>{fanData.name}</h2>
-            <p>{fanData.location}</p>
-            <p>{fanData.description}</p>
+          <h2>{fan.name}</h2>
+          <p>{fan.location}</p>
+          <p>{fan.description}</p>
         </figcaption>
       </figure>
       <article>
-        {
-          customerReleases?.map((release, index) => (
-            <AlbumOverview key={index} props={release}/>
-          ))
-        }
+        {customerReleases?.map((release, index) => (
+          <AlbumOverview key={index} props={release} />
+        ))}
       </article>
     </section>
   );
